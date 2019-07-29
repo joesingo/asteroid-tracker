46936ffc1a33d17ecffbe23578097d6d2d90de2f
Joe Singleton <joesingo@gmail.com>
2019-07-29 15:21:33 +0100

    Add test for get_pages()
    For #1

--- asteroid_tracker/tests.py
+++ asteroid_tracker/tests.py
@@ -1,9 +1,12 @@
+from unittest.mock import patch
+
 import pytest
 import yaml
 
 from asteroid_tracker import (
     InvalidConfigError,
     Config,
+    Page,
     SiteBuilder,
     Target,
     TomConnectionError
@@ -55,6 +58,10 @@ class TestSiteBuilder:
             targets=[dict(pk=1, template=1, preview_image="img", teaser="teaser")]
         )
 
+    @pytest.fixture
+    def builder(self, config):
+        return SiteBuilder(config)
+
     def test_url_trailing_slash(self, config):
         # Trailing slash should be removed
         config.tom_education_url = "http://slash.net/"
@@ -71,3 +78,51 @@ class TestSiteBuilder:
         with pytest.raises(TomConnectionError) as excinfo:
             builder.build_site(tmp_path)
             assert excinfo.value == "Could not connect to TOM at 'somethingmadeup'"
+
+    def test_get_pages(self, config):
+        config.targets = [
+            Target(pk=100, template=1, preview_image="img.png", teaser="hello"),
+            Target(pk=101, template=1, preview_image="img.png"),
+        ]
+        builder = SiteBuilder(config)
+
+        with patch("asteroid_tracker.build_site.requests") as mock:
+            class FakeResponse:
+                times_called = 0
+                def json(self):
+                    self.times_called += 1
+                    return {
+                        "target": {
+                            "identifier": f"target_{self.times_called}",
+                            "name": "Cool target"
+                        }
+                    }
+
+            mock.get.return_value = FakeResponse()
+            pages = list(builder.get_pages())
+
+        assert len(pages) == 3
+        # Check names
+        assert pages[0].name == "target_1"
+        assert pages[1].name == "target_2"
+        assert pages[2].name == ""  # home page
+        # Check API URLs in context
+        assert pages[0].context["api_url"].endswith("/100/")
+        assert pages[1].context["api_url"].endswith("/101/")
+        # Check templates
+        assert pages[0].template == pages[1].template
+        assert pages[0].template != pages[2].template
+        # Check home page context
+        assert pages[2].context == {
+            "targets": [{
+                "url": "/target_1",
+                "name": "Cool target",
+                "image_name": "100.png",
+                "teaser": "hello",
+            }, {
+                "url": "/target_2",
+                "name": "Cool target",
+                "image_name": "101.png",
+                "teaser": "",
+            }]
+        }
